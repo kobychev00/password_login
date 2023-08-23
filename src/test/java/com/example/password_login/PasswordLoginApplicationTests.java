5f173e2ebe71a6fb3fe68e4bbee4c7bdeac04eb9
air <kobychev00@bk.ru>
2023-08-23 20:18:37 +0300

    23.08.2023 20:17

---
+++ src/test/java/com/example/password_login/PasswordLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.password_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
